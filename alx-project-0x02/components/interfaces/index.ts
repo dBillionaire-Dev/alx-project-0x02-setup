ed5a8ae0de55fb02d41bb887451ec9259a457e6c
dBillioniare-Dev <nezerekunke@gmail.cm>
2025-06-05 04:11:24 +0100

    task_1

---
+++ alx-project-0x02/components/interfaces/index.ts
@@ -0,0 +1,7 @@
+#Empty file to ensure the directory is treated as a module
+export * from './IComponent';
+export * from './IComponentProps';
+export * from './IComponentState';
+export * from './IComponentContext';
+export * from './IComponentEvent';
+export * from './IComponentLifecycle';  
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/layouts/Header";
+import Header from "@/components/layout/Header";
 
 const index = () => {
   return (
